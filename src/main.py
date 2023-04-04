27e2bc07d29684d3c648083c3e38c301e226024c
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2023-04-04 14:47:47 +0200

    Organized tuning and fine-tuning in MLFlow runs and nested runs

--- src/main.py
+++ src/main.py
@@ -15,6 +15,10 @@ from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, accuracy_s
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 
 
+def xor(a: bool, b: bool) -> bool:
+    return (a and not b) or (b and not a)
+
+
 @hydra.main(version_base='1.3', config_path='../config', config_name='params')
 def main(params: DictConfig) -> None:
     """
@@ -37,7 +41,7 @@ def main(params: DictConfig) -> None:
     if params.transformers.seed is not None:
         transformers.set_seed(params.transformers.seed)
 
-    ''' Set various directories '''
+    ''' Set various path '''
 
     # Absolute path to the repo root in the local filesystem
     repo_root = Path('..').resolve()
@@ -49,15 +53,20 @@ def main(params: DictConfig) -> None:
     # Absolute path to the directory where model and model checkpoints are be saved and loaded from
     models_path = (repo_root / params.main.models_dir).resolve()
 
+    # Model to be loaded; if not found, a model is optimized and then saved there
+    fine_tuned_model_path = models_path / params.main.fine_tuned_model_dir
+
+    best_trial_path = models_path / 'best_trial.yaml'
+
     ''' If there is no MLFlow run currently ongoing, then start one '''
 
     # If the script has been started from shell with `mflow run` then a run is ongoing already, no need to start it
-    mf.start_run()
+    if mf.active_run() is None:
+        info('No active MLFlow run, starting one now')
+        mf.start_run()
+
     mf_run = mf.active_run()
     info(f"Active run name is: {mf_run.info.run_name}")
-
-    # Model to be loaded; if not found, a model is optimized and then saved there
-    fine_tuned_model_path = models_path / params.main.fine_tuned_model_dir
 
     # Check GPU is available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -73,45 +82,11 @@ def main(params: DictConfig) -> None:
 
     emotions_encoded = emotions.map(tokenize, batched=True, batch_size=None)
 
-    num_labels = 6
-    already_trained = False
+    num_labels = 6  # Yuck!
 
-    if Path(fine_tuned_model_path).exists():
-        info(f'Loading model from directory f{fine_tuned_model_path}')
-        model = (AutoModelForSequenceClassification.from_pretrained(fine_tuned_model_path).to(device))
-        already_trained = True
-    else:
-        print(f'Model f{fine_tuned_model_path} not found, going to download pre-trained model for fine-tuning')
-        model = None
-        # model = (AutoModelForSequenceClassification.from_pretrained(pretrained_model, num_labels=num_labels).to(device))
-
-    print(model)
-
-    # logging_steps = len(emotions_encoded["train"]) // params.transformers.batch_size
     info(f'Training set contains {len(emotions_encoded["train"])} samples')
     model_name = f"{pretrained_model}-finetuned-emotion"
     output_dir = str(models_path / model_name)
-
-    training_args = TrainingArguments(output_dir=output_dir,
-                                      num_train_epochs=params.transformers.epochs,
-                                      learning_rate=2e-5,
-                                      per_device_train_batch_size=params.transformers.batch_size,
-                                      per_device_eval_batch_size=params.transformers.batch_size,
-                                      weight_decay=0.01,
-                                      evaluation_strategy="epoch",
-                                      disable_tqdm=False,
-                                      # logging_steps=logging_steps,
-                                      push_to_hub=False,
-                                      log_level="error",
-                                      logging_strategy='epoch',
-                                      report_to=['mlflow'],
-                                      logging_first_step=True,
-                                      save_strategy='epoch')
-
-    def model_init(trial: opt.trial.Trial):
-        the_model = AutoModelForSequenceClassification.from_pretrained(pretrained_model, num_labels=num_labels).to(
-            device)
-        return the_model
 
     def compute_metrics(pred):
         ground_truth = pred.label_ids
@@ -120,85 +95,138 @@ def main(params: DictConfig) -> None:
         acc = accuracy_score(ground_truth, preds)
         return {"accuracy": acc, "f1": f1}
 
-    trainer = Trainer(model=model,
-                      args=training_args,
-                      compute_metrics=compute_metrics,
-                      train_dataset=emotions_encoded["train"],
-                      eval_dataset=emotions_encoded["validation"],
-                      tokenizer=tokenizer,
-                      model_init=model_init)
+    def optimize(model, overriding_params=None, model_init=None, hp_space=None):
+        assert xor(model is not None, model_init is not None or hp_space is not None)
 
-    if not already_trained:
-        def hp_space(trial: opt.trial.Trial) -> dict:
-            res = {
-                "learning_rate": trial.suggest_float("learning_rate", 1e-6, 1e-4, log=True),
-                "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size", [16, 32, 64]),
-            }
-            return res
+        training_args = TrainingArguments(output_dir=output_dir,
+                                          num_train_epochs=params.transformers.epochs,
+                                          learning_rate=2e-5,
+                                          per_device_train_batch_size=params.transformers.batch_size,
+                                          per_device_eval_batch_size=params.transformers.batch_size,
+                                          weight_decay=0.01,
+                                          evaluation_strategy="epoch",
+                                          disable_tqdm=False,
+                                          push_to_hub=False,
+                                          log_level="error",
+                                          logging_strategy='epoch',
+                                          report_to=['mlflow'],
+                                          logging_first_step=True,
+                                          save_strategy='epoch')
+
+        if overriding_params is not None:
+            for key, value in overriding_params.items():
+                setattr(training_args, key, value)
+
+        trainer = Trainer(model=model,
+                          args=training_args,
+                          compute_metrics=compute_metrics,
+                          train_dataset=emotions_encoded["train"],
+                          eval_dataset=emotions_encoded["validation"],
+                          tokenizer=tokenizer,
+                          model_init=model_init)
 
         def compute_objective(metrics: dict) -> float:
             return metrics['eval_f1']
 
-        best_run = trainer.hyperparameter_search(hp_space=hp_space,
-                                                 n_trials=3,
-                                                 direction='maximize',
-                                                 compute_objective=compute_objective)
-        # print(training_metrics)
-        info(f'Best run: {best_run}')
-        trainer.save_model(fine_tuned_model_path)
-        already_trained = True
+        if model_init is not None:
+            res = trainer.hyperparameter_search(hp_space=hp_space,
+                                                n_trials=params.main.tuning_trials,
+                                                direction='maximize',
+                                                compute_objective=compute_objective)
+            # print(training_metrics)
+            info(f'Best run: {res}')
+        else:
+            res = trainer.train()
+            info(f'Fine tuning results: {res}')
 
-    def plot_confusion_matrix(y_preds, y_true, labels, show=True):
-        cm = confusion_matrix(y_true, y_preds, normalize="true")
-        fig, ax = plt.subplots(figsize=(6, 6))
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
-        disp.plot(cmap="Blues", values_format=".2f", ax=ax, colorbar=False)
-        plt.title("Normalized confusion matrix")
-        if show:
-            plt.show()
-        return fig
+        return res, trainer
 
-    labels = emotions["train"].features["label"].names
+    def model_init(trial: opt.trial.Trial | None = None):
+        the_model = AutoModelForSequenceClassification.from_pretrained(pretrained_model, num_labels=num_labels).to(
+            device)
+        return the_model
 
-    info(f'Validation set contains {len(emotions_encoded["validation"])} samples')
-    preds_output_val = trainer.predict(emotions_encoded["validation"])
-    info(f'Validation metrics\n{preds_output_val.metrics}')
+    if params.train.tune:
+        with mf.start_run(nested=True, description='hyperparemeters tuning') as _:
+            def hp_space(trial: opt.trial.Trial) -> dict:
+                res = {
+                    "learning_rate": trial.suggest_float("learning_rate", 1e-6, 1e-4, log=True),
+                    "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size",
+                                                                             [16, 32, 64]),
+                }
+                return res
 
-    y_preds_val = np.argmax(preds_output_val.predictions, axis=1)
-    y_valid = np.array(emotions_encoded["validation"]["label"])
+            best_trial, _ = optimize(model=None, overriding_params=None, model_init=model_init, hp_space=hp_space)
+            OmegaConf.save(best_trial.hyperparameters, best_trial_path)
+    if params.train.fine_tune:
+        with mf.start_run(nested=True, description='pre-trained model fine-tuning') as _:
+            best_trial_params = None
+            if Path(best_trial_path).exists():
+                info(f'Loading tuned hyper-parameters from {best_trial_path}')
+                best_trial_params = dict(OmegaConf.load(best_trial_path))
+            # info(f'Loading model from directory f{fine_tuned_model_path}')
+            model = model_init()
 
-    fig_val = plot_confusion_matrix(y_preds_val, y_valid, labels, False)
-    mf.log_figure(fig_val, 'validation_confusion_matrix.png')
+            training_metrics, trainer = optimize(model=model, overriding_params=best_trial_params)
 
-    info(f'Test set contains {len(emotions_encoded["test"])} samples')
-    preds_output_test = trainer.predict(emotions_encoded["test"])
-    info(f'Test metrics\n{preds_output_test.metrics}')
-    mf.log_metrics(preds_output_test.metrics)
+            trainer.save_model(fine_tuned_model_path)
 
-    y_preds_test = np.argmax(preds_output_test.predictions, axis=1)
-    y_test = np.array(emotions_encoded["test"]["label"])
+            def plot_confusion_matrix(y_preds, y_true, labels, show=True):
+                cm = confusion_matrix(y_true, y_preds, normalize="true")
+                fig, ax = plt.subplots(figsize=(6, 6))
+                disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+                disp.plot(cmap="Blues", values_format=".2f", ax=ax, colorbar=False)
+                plt.title("Normalized confusion matrix")
+                if show:
+                    plt.show()
+                return fig
 
-    fig_test = plot_confusion_matrix(y_preds_test, y_test, labels, False)
-    mf.log_figure(fig_test, 'test_confusion_matrix.png')
+            labels = emotions["train"].features["label"].names
+
+            info(f'Validation set contains {len(emotions_encoded["validation"])} samples')
+            preds_output_val = trainer.predict(emotions_encoded["validation"])
+            info(f'Validation metrics\n{preds_output_val.metrics}')
+
+            y_preds_val = np.argmax(preds_output_val.predictions, axis=1)
+            y_valid = np.array(emotions_encoded["validation"]["label"])
+
+            fig_val = plot_confusion_matrix(y_preds_val, y_valid, labels, False)
+            mf.log_figure(fig_val, 'validation_confusion_matrix.png')
+
+        with mf.start_run(nested=True, description='fine-tuned model testing') as _:
+            # model = AutoModelForSequenceClassification.from_pretrained(fine_tuned_model_path).to(device)
+            # tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
+            # pipe = pipeline(model=model, task='text-classification', tokenizer=tokenizer)
+            info(f'Test set contains {len(emotions_encoded["test"])} samples')
+            preds_output_test = trainer.predict(emotions_encoded["test"])
+            # preds_output_test = pipe(emotions_encoded["test"])
+            info(f'Test metrics\n{preds_output_test.metrics}')
+            mf.log_metrics(preds_output_test.metrics)
+
+            y_preds_test = np.argmax(preds_output_test.predictions, axis=1)
+            y_test = np.array(emotions_encoded["test"]["label"])
+
+            fig_test = plot_confusion_matrix(y_preds_test, y_test, labels, False)
+            mf.log_figure(fig_test, 'test_confusion_matrix.png')
 
 
 if __name__ == '__main__':
     main()
 
 """
-TODO
-Add a test step -> Done 
-Turn the script into an MLFlow project -> Done
-Store hyperparameters in a config. file -> Done
+TODO Add a test step -> Done
+Turn the  script into an MLFlow project -> Done
+Store hyperparameters in a config.file -> Done
 Introduce proper logging -> Done
-Tune hyperparameters (using some framework/library) -> Done
-(Re-)train and save the best model after hyperparameters tuning
-Version the saved model (also the dataset?)
+Tune hyperparameters(using some framework / library) -> Done
+(Re -)train and save -> Done
+Version the saved model(also the dataset?)
 Draw charts of the training and validation loss and the confusion matrix under MLFlow -> Done
 Follow Andrej recipe
 Plot charts to MLFlow for debugging of the training process, as per Andrej's lectures
 Implement early stopping
 Give the model an API, deploy it
-Make a GUI via gradio and/or streamlit
-
+Make a GUI via gradio and / or streamlit
+Optimize hyper-parameters tuning such that it saves the best model so far at every trial, so it doesn't have to be
+    computed again later
 """
